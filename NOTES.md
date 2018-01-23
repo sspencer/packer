af068d4b13698e886d4ffaa186f7857a8e3e536b
Steve Spencer <sspencer@playnetwork.com>
2018-01-22 20:42:46 -0800

    from glide to dep.  thinking about command line only, no config file

--- NOTES.md
+++ NOTES.md
@@ -1,4 +1,3 @@
-Use [glide](https://github.com/Masterminds/glide/) to manage packages.
 
 Use Cobra to manage config files / flags.
 
--- layout.go
+++ layout.go
@@ -1,13 +1,16 @@
 package packer
 
-// Layout is used to sort sprites in different ways to achieve differnt packs.
+// Layout is used to sort sprites in different ways to achieve different packs.
 //go:generate stringer -type=Layout
 type Layout int
 
 const (
-	// LayoutByWidth and others describe sort to use when laying out image
+	// LayoutByWidth sorts objects by width
 	LayoutByWidth Layout = iota
+	// LayoutByHeight sorts objects by height
 	LayoutByHeight
+	// LayoutByArea sorts objects by total area
 	LayoutByArea
+	// LayoutByMax sorts objects by max dimension, width or height
 	LayoutByMax
 )
